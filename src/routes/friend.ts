1eabf194ef95766036e7b4ca36ac0efbc0c8094b
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2023-07-08 20:12:30 -0500

    Project Setup

---
+++ src/routes/friend.ts
@@ -0,0 +1,61 @@
+/**
+ * Express Router middeware for Friend APIs
+ *
+ * @author Hyecheol (Jerry) Jang
+ */
+
+import * as express from 'express';
+
+// Path: /friend
+const friendRouter = express.Router();
+
+// GET: /friend
+// friendRouter.get('/', async (req, res, next) => {
+//   // TODO;
+// });
+
+// DELETE: /friend/{base64Email}
+// friendRouter.delete('/:base64Email', async (req, res, next) => {
+//   // TODO;
+// });
+
+// POST: /friend/request
+// friendRouter.post('/request', async (req, res, next) => {
+//   // TODO;
+// });
+
+// GET: /friend/request/received
+// friendRouter.get('/request/received', async (req, res, next) => {
+//   // TODO;
+// });
+
+// DELETE: /friend/request/received/{friendRequestId}
+// friendRouter.delete(
+//   '/request/received/:friendRequestId',
+//   async (req, res, next) => {
+//     // TODO;
+//   }
+// );
+
+// POST: /friend/request/received/{friendRequestId}/accept
+// friendRouter.post(
+//   '/request/received/:friendRequestId/accept',
+//   async (req, res, next) => {
+//     // TODO;
+//   }
+// );
+
+// GET: /friend/request/sent
+// friendRouter.get('/request/sent', async (req, res, next) => {
+//   // TODO;
+// });
+
+// DELETE /friend/request/sent/{friendRequestId}
+// friendRouter.delete(
+//   '/request/sent/:friendRequestId',
+//   async (req, res, next) => {
+//     // TODO;
+//   }
+// );
+
+export default friendRouter;
